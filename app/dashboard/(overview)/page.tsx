afe6d6f1ea4dc302b597361c3a5dd6ab5e90aac7
NiloufarSoltani <niloufarsoltani1993@gmail.com>
2024-07-25 13:33:12 -0400

    remove eslint server component error

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -17,18 +17,15 @@ export default async function Page() {
         Dashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        <Suspense fallback={<CardsSkeleton/>}>
-          {/* @ts-expect-error Server Component */}
+        <Suspense fallback={<CardsSkeleton/>}> 
           <CardWrapper />
         </Suspense>
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        <Suspense fallback={<RevenueChartSkeleton />}>
-          {/* @ts-expect-error Server Component */}
+        <Suspense fallback={<RevenueChartSkeleton />}> 
           <RevenueChart />
         </Suspense>
-        <Suspense fallback={<LatestInvoicesSkeleton />}>
-          {/* @ts-expect-error Server Component */}
+        <Suspense fallback={<LatestInvoicesSkeleton />}> 
           <LatestInvoices />
         </Suspense>
       </div>
--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -29,8 +29,7 @@ export default async function Customers({
   return (
     <div className="w-full">
       <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
-        <Suspense fallback={<p>loading ...</p>}>
-          {/* @ts-expect-error Server Component */}
+        <Suspense fallback={<p>loading ...</p>}> 
           <Table customers={customers} />
         </Suspense>
       </div>
