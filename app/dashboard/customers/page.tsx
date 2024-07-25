2983ed494555ae529cbc12697b8791b7c61ef988
NiloufarSoltani <niloufarsoltani1993@gmail.com>
2024-07-24 23:20:03 -0400

    Fix pagination bug and add customer section

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,35 @@
-export default function Customers(){
-    return <p>Customers Page</p>
+import { Metadata } from "next";
+import { lusitana } from "@/app/ui/fonts";
+import Table from "@/app/ui/customers/table";
+import { fetchFilteredCustomers } from "@/app/lib/data";
+import { Suspense } from "react";
+// import { customers } from "@/app/lib/placeholder-data";
+
+export const metadata: Metadata = {
+  title: "Customers",
+};
+
+export default async function Customers({
+  searchParams,
+}: {
+  searchParams?: {
+    page?: string;
+    query?: string;
+  };
+}) {
+  const query = searchParams?.query || "";
+  const currentPage = Number(searchParams?.page) || 1;
+  const customers = await fetchFilteredCustomers(query);
+  
+  return (
+    <div className="w-full">
+      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+        {/* <Search placeholder="Search customers..." /> */}
+        <Suspense fallback={<p>loading ...</p>}>
+          {/* @ts-expect-error Server Component */}
+          <Table customers={customers} />
+        </Suspense>
+      </div>
+    </div>
+  );
 }
--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -12,7 +12,7 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
   const currentPage = Number(searchParams.get('page')) || 1;
 
   function createPageURL(pageNumber: string|number){
-    const params = new URLSearchParams();
+    const params = new URLSearchParams(searchParams);
     params.set('page', pageNumber.toString());
     return `${pathname}?${params.toString()}`
   }
