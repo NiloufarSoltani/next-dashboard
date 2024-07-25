1062060429790dafd09e12a9181ef71253821d8d
NiloufarSoltani <niloufarsoltani1993@gmail.com>
2024-07-24 23:47:33 -0400

    fix eslint bugs

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -16,13 +16,11 @@ export default async function Customers({
   };
 }) {
   const query = searchParams?.query || "";
-  const currentPage = Number(searchParams?.page) || 1;
   const customers = await fetchFilteredCustomers(query);
   
   return (
     <div className="w-full">
       <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
-        {/* <Search placeholder="Search customers..." /> */}
         <Suspense fallback={<p>loading ...</p>}>
           {/* @ts-expect-error Server Component */}
           <Table customers={customers} />
